4bafbc1e6bd565cc8997ebec26566688e5ff7391
Gustavo Silva <gustavo.silva@codificar.com.br>
2021-04-14 21:20:38 -0300

    Clean Index

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -42,81 +42,21 @@
 
 <script>
 import Layout from '../../Shared/Layout'
-import { ref } from 'vue'
-import { Dialog, DialogOverlay, Menu, MenuButton, MenuItem, MenuItems, TransitionChild, TransitionRoot } from '@headlessui/vue'
-import { BellIcon, ClockIcon, CogIcon, CreditCardIcon, DocumentReportIcon, HomeIcon, MenuAlt1Icon, QuestionMarkCircleIcon, ScaleIcon, ShieldCheckIcon, UserGroupIcon, XIcon } from '@heroicons/vue/outline'
-import { CashIcon, CheckCircleIcon, ChevronDownIcon, ChevronRightIcon, OfficeBuildingIcon, SearchIcon } from '@heroicons/vue/solid'
+import { ScaleIcon } from '@heroicons/vue/outline'
 
-const navigation = [
-  { name: 'Home', href: '#', icon: HomeIcon, current: true },
-  { name: 'History', href: '#', icon: ClockIcon, current: false },
-  { name: 'Balances', href: '#', icon: ScaleIcon, current: false },
-  { name: 'Cards', href: '#', icon: CreditCardIcon, current: false },
-  { name: 'Recipients', href: '#', icon: UserGroupIcon, current: false },
-  { name: 'Reports', href: '#', icon: DocumentReportIcon, current: false },
-]
-const secondaryNavigation = [
-  { name: 'Settings', href: '#', icon: CogIcon },
-  { name: 'Help', href: '#', icon: QuestionMarkCircleIcon },
-  { name: 'Privacy', href: '#', icon: ShieldCheckIcon },
-]
 const cards = [
   { name: 'Account balance', href: '#', icon: ScaleIcon, amount: '$30,659.45' },
   { name: 'Account balance', href: '#', icon: ScaleIcon, amount: '$30,659.45' },
   { name: 'Account balance', href: '#', icon: ScaleIcon, amount: '$30,659.45' },
-  // More items...
 ]
-const transactions = [
-  {
-    id: 1,
-    name: 'Payment to Molly Sanders',
-    href: '#',
-    amount: '$20,000',
-    currency: 'USD',
-    status: 'success',
-    date: 'July 11, 2020',
-    datetime: '2020-07-11',
-  },
-  // More transactions...
-]
-const statusStyles = {
-  success: 'bg-green-100 text-green-800',
-  processing: 'bg-yellow-100 text-yellow-800',
-  failed: 'bg-gray-100 text-gray-800',
-}
 
 export default {
   metaInfo: { title: 'Users' },
   layout: Layout,
-  components: {
-    Dialog,
-    DialogOverlay,
-    Menu,
-    MenuButton,
-    MenuItem,
-    MenuItems,
-    TransitionChild,
-    TransitionRoot,
-    BellIcon,
-    CashIcon,
-    CheckCircleIcon,
-    ChevronDownIcon,
-    ChevronRightIcon,
-    MenuAlt1Icon,
-    OfficeBuildingIcon,
-    SearchIcon,
-    XIcon,
-  },
+  components: {},
   setup() {
-    const sidebarOpen = ref(false)
-
     return {
-      navigation,
-      secondaryNavigation,
       cards,
-      transactions,
-      statusStyles,
-      sidebarOpen,
     }
   },
 }
